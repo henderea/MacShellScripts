6398853f37b878e722eecd3a5636f0f4e578b9c3
Eric Henderson <henderea@gmail.com>
2013-04-25 14:16:20 -0400

    add kmeans stuff

---
+++ bin/lib/histogram.rb
@@ -0,0 +1,39 @@
+$LOAD_PATH << File.dirname(__FILE__)
+
+require 'maputil'
+
+module Enumerable
+  def histogram(ks = nil, width = 100, height = 50)
+    mi = min
+    ma = max
+    diff = ma - mi
+    step = diff.to_f / width.to_f
+    counts = []
+    (0...width).each { |i|
+      counts[i] = 0
+    }
+    each { |v|
+      i = ((v - mi).to_f / (step + 1).to_f).floor
+      counts[i] += 1
+    }
+    max_y = counts.max
+    lines = []
+    (0...height).each { |i|
+      lines[i] = ' ' * width
+    }
+    (0...width).each { |i|
+      h = ((counts[i].to_f / max_y.to_f) * height.to_f).round
+      ((height - h)...height).each { |j|
+        lines[j][i] = '#'
+      }
+    }
+    unless ks.nil?
+      lines[height] = ' ' * width
+      ks.each { |v|
+        i = ((v - mi) / step).to_i
+        lines[height][i] = '|'
+      }
+    end
+    lines
+  end
+end
---
+++ bin/lib/kmeans.rb
@@ -0,0 +1,116 @@
+$LOAD_PATH << File.dirname(__FILE__)
+
+require 'maputil'
+
+def f_test(clusters, means, cnt, avg)
+  ev = 0
+  (0...means.count).each { |i|
+    ni = clusters[i].count
+    ev += ni * ((means[i] - avg) ** 2)
+  }
+  ev /= means.count - 1
+  uv = 0
+  (0...means.count).each { |i|
+    (0...clusters[i].count).each { |j|
+      uv += (clusters[i][j] - means[i]) * (clusters[i][j] - means[i])
+    }
+  }
+  uv /= (cnt - means.count)
+  (ev / uv)
+end
+
+def f_test2(clusters, means, cnt)
+  uv = 0
+  (0...means.count).each { |i|
+    tmp = 0
+    (0...clusters[i].count).each { |j|
+      tmp += (clusters[i][j] - means[i]) ** 2
+    }
+    tmp /= clusters[i].count
+    uv  += Math.sqrt(tmp)
+  }
+  (uv / (cnt - means.count))
+end
+
+module Enumerable
+  def nmeans(max_k = 10, threshold = 0.05)
+    su  = sum
+    cnt = count
+    avg = su / cnt
+    kso = kmeans(1)
+    cso = get_clusters(kso)
+    fto = f_test2(cso, kso, cnt)
+    ks  = kmeans(2)
+    cs  = get_clusters(ks)
+    ft  = f_test(cs, ks, cnt, avg)
+    ft2 = f_test2(cs, ks, cnt)
+    #puts kso.inspect
+    #puts ks.inspect
+    #puts cso.inspect
+    #puts cs.inspect
+    #puts fto
+    #puts ft2
+    if ft2 >= fto
+      return kso
+    end
+    (3..max_k).each { |k|
+      kso = ks
+      fto = ft
+      ks  = kmeans(k)
+      #puts ks.inspect
+      cs  = get_clusters(ks)
+      ft  = f_test(cs, ks, cnt, avg)
+      if ((ft - fto) / fto) < threshold
+        return kso
+      end
+    }
+    ks
+  end
+
+  def kmeans(k)
+    mi   = min
+    ma   = max
+    diff = ma - mi
+    ks   = []
+    (1..k).each { |i|
+      ks[i - 1] = mi + (i * (diff / (k + 1)))
+    }
+    kso = false
+    while ks != kso
+      kso      = ks
+      clusters = get_clusters(kso)
+      ks = []
+      clusters.each_with_index { |val, key|
+        cnt = val.count
+        if cnt <= 0
+          ks[key] = kso[key]
+        else
+          sum = val.sum
+          ks[key] = (sum / cnt)
+        end
+      }
+      ks.sort
+    end
+    ks
+  end
+
+  def get_clusters(means)
+    clusters = []
+    (0...means.count).each { |i|
+      clusters[i] = []
+    }
+    each { |item|
+      cluster  = false
+      distance = false
+      (0...means.count).each { |i|
+        diff = (means[i] - item).abs
+        if distance == false || diff < distance
+          cluster  = i
+          distance = diff
+        end
+      }
+      clusters[cluster][clusters[cluster].count] = item
+    }
+    clusters
+  end
+end
