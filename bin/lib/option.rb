14050d9d52379e22324ab98aec85f2beba766cc2
Eric Henderson <henderea@gmail.com>
2013-06-15 19:30:39 -0400

    do some refactoring

--- bin/lib/option.rb
+++ bin/lib/option.rb
@@ -4,11 +4,11 @@ module Option
   end
 
   def self.add_option_with_param(options, opts, short_name, long_name, opt_name)
-    opts.on(short_name, long_name) { |name| options[opt_name] = name }
+    opts.on(short_name, long_name) { |param| options[opt_name] = param }
   end
 
   def self.add_option_with_param_and_type(options, opts, short_name, long_name, opt_name, type)
-    opts.on(short_name, long_name, type) { |name| options[opt_name] = name }
+    opts.on(short_name, long_name, type) { |param| options[opt_name] = param }
   end
 
   def self.add_option_single(options, opts, name, opt_name)
@@ -16,11 +16,11 @@ module Option
   end
 
   def self.add_option_with_param_single(options, opts, name, opt_name)
-    opts.on(name) { |name| options[opt_name] = name }
+    opts.on(name) { |param| options[opt_name] = param }
   end
 
   def self.add_option_with_param_and_type_single(options, opts, name, opt_name, type)
-    opts.on(name, type) { |name| options[opt_name] = name }
+    opts.on(name, type) { |param| options[opt_name] = param }
   end
 
   def self.add_option_block(options, opts, short_name, long_name, opt_name, &block)
@@ -28,11 +28,11 @@ module Option
   end
 
   def self.add_option_with_param_block(options, opts, short_name, long_name, opt_name, &block)
-    opts.on(short_name, long_name) { |name| options[opt_name] = name; block.call }
+    opts.on(short_name, long_name) { |param| options[opt_name] = param; block.call }
   end
 
   def self.add_option_with_param_and_type_block(options, opts, short_name, long_name, opt_name, type, &block)
-    opts.on(short_name, long_name, type) { |name| options[opt_name] = name; block.call }
+    opts.on(short_name, long_name, type) { |param| options[opt_name] = param; block.call }
   end
 
   def self.add_option_single_block(options, opts, name, opt_name, &block)
@@ -40,10 +40,10 @@ module Option
   end
 
   def self.add_option_with_param_single_block(options, opts, name, opt_name, &block)
-    opts.on(name) { |name| options[opt_name] = name; block.call }
+    opts.on(name) { |param| options[opt_name] = param; block.call }
   end
 
   def self.add_option_with_param_and_type_single_block(options, opts, name, opt_name, type, &block)
-    opts.on(name, type) { |name| options[opt_name] = name; block.call }
+    opts.on(name, type) { |param| options[opt_name] = param; block.call }
   end
 end
