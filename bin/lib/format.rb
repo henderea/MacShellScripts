4a19e502c9157248d27a354b020689711e490c77
Eric Henderson <henderea@gmail.com>
2014-01-24 12:28:46 -0500

    start to use the everyday-cli-utils gem

--- bin/lib/format.rb
+++ bin/lib/format.rb
@@ -90,3 +90,17 @@ module Format
     self::format(text, self::build_string(true, true, fgcolor, bgcolor))
   end
 end
+
+class String
+  alias :old_method_missing :method_missing
+
+  def method_missing(method, *args)
+    name   = method.to_s
+    colors = 'black|red|green|yellow|blue|purple|cyan|white|none'
+    if (name =~ /format(_bold)?(_underline)?(?:_fg_(#{colors}))?(?:_bg_(#{colors}))?/).nil?
+      old_method_missing(method, *args)
+    else
+      Format::format(self, Format::build_string(!$1.nil?, !$2.nil?, $3.nil? ? nil : $3.to_sym, $4.nil? ? nil : $4.to_sym))
+    end
+  end
+end
